8572fd96d9fab75445f5195266cdac569304cb0c
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-08 21:42:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -18,7 +18,7 @@ class Customer
     @@all << self 
   end 
   
-  def new_meal(name, waiter, total, tip)
-    Meal.new(name, waiter, total, tip)
+  def new_meal(self, waiter, total, tip)
+    Meal.new(self, waiter, total, tip)
   end 
 end
