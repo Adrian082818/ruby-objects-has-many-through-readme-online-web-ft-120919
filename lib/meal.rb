60f6879a8e991d99ab46ec7ca035da1ea3b48878
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-08 21:36:06 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,13 @@
 class Meal
 
+attr_accessor :waiter, :customer, :total, :tip 
+
+def initialize(name, customer, total, tip)
+  @name = name 
+  @customer =customer
+  @total = total
+  @tip = tip 
+end 
+
+
 end
